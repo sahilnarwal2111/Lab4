eb702d76cfacd9024f68f0ba95bbb363ae1afcb1
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-02-12 12:22:31 +0530

    first commit

---
+++ demo/src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class MainTest {
+    @Test
+    public static void FirstTest(){
+        System.out.println("FirstTest");
+    }
+    @Test
+    public static void SecondTest(){
+        System.out.println("SecondTest");
+    }
+    @Test
+    public static void ThirdTest(){
+        System.out.println("ThirdTest");
+    }
+
+
+}
---
+++ demo/src/test/java/SecondTests.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class SecondTests {
+    @Test
+    public static void TestFromSecondFile(){
+        System.out.println("TestFromSecondFile");
+    }
+
+}
