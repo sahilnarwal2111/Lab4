2003917691d62900a4bb0b429a68093c980fdd64
sahilnarwal2111 <500097751@stu.upes.ac.in>
2024-05-05 18:06:36 +0530

    Learning

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,10 +1,16 @@
 package com.example;
 
-
-
+import MainTest;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        
+        MainTest test = new MainTest();
+        test.FirstTest();
+        test.SecondTest();
+
+
+
     }
 }
--- demo/src/test/java/MainTest.java
+++ demo/src/test/java/MainTest.java
@@ -1,18 +1,45 @@
+
+import java.util.Scanner;
+
 import org.testng.annotations.Test;
 
 public class MainTest {
     @Test
     public static void FirstTest(){
-        System.out.println("FirstTest");
+        System.out.println("Enter email: \n");
+        Scanner sc = new Scanner(System.in);
+        //Ensure that @ is present in email for logging in
+        String str = sc.nextLine();
+        if(str.contains("@")){
+            System.out.println("Email is valid");
+        }
+        else{
+            System.out.println("Email is invalid");
+        }
     }
     @Test
     public static void SecondTest(){
-        System.out.println("SecondTest");
+        //Ensure that password is atleast 8 characters long
+        System.out.println("Enter password: \n");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        if(str.length() >= 8){
+            System.out.println("Password is valid");
+        }
+        else{
+            System.out.println("Password is invalid");
+        }
     }
     @Test
     public static void ThirdTest(){
         System.out.println("ThirdTest");
     }
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+        FirstTest();
+        SecondTest();
+        // ThirdTest();
+    }
 
 }
